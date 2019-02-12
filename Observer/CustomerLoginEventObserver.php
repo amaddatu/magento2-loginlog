324e4b1449929e3f15d9af892eb66495b6ebca9f
Anthony Maddatu <amaddatu@gmail.com>
2019-02-11 19:31:20 -0500

    Add necessary functions to read the remote address

---
+++ Observer/CustomerLoginEventObserver.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Customer Login Observer
+ * 
+ * Copyright © Anthony Maddatu 
+ */
+namespace Amaddatu\LoginLog\Observer;
+
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class CustomerLoginEventObserver implements ObserverInterface
+{
+    /**
+     *  @var \Magento\Framework\App\Helper\Context this variable is used to avoid a possible deprecation issues with inheritance based classes
+     */
+    protected $_context;
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context this will be used to grab both the remote ip and the logger
+     */
+    public function __construct(
+        Context $context
+    ){
+        $this->_context = $context;
+    }
+    /**
+     * @param \Magento\Framework\Event\Observer $observer this should hold the customer object
+     */
+    public function execute(Observer $observer)
+    {
+        // $logger = $this->_context->getLogger();
+        // $logger->addDebug("Customer Login Event");
+        $customer = $observer->getEvent()->getCustomer();
+        $remote_address = $this->_context->getRemoteAddress()->getRemoteAddress();
+        // $logger->addDebug($customer->getName()); //Get customer name
+
+        // echo get_class($logger);
+        // echo $customer->getId() . "<br/>";
+        // echo $this->_context->getRemoteAddress()->getRemoteAddress();
+        // echo "event observed: " . $customer->getName();
+        // exit;
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Creates a customer loginlog for the customer.
 
 ## linux commands used to find the necessary pieces
 
-`grep --include=*.php --include=*.phtml -rnw 'dispatch' module-customer`
+`grep --include=*.php --include=*.phtml -rnw 'dispatch' web/vendor/magento/module-customer`
+
+`grep --include=*.php --include=*.phtml -rnw 'IP address' web/vendor/magento/module-sales`
+
+`grep --include=*.php --include=*.phtml -rnw 'RemoteAddress' web/vendor/magento/`
